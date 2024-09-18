c9fc3476a903cd4eae63e1154301f4b7d2c2b7aa
danielm-LCWC <144791876+danielm-LCWC@users.noreply.github.com>
2024-09-18 09:48:30 +0000

    task

--- task.py
+++ task.py
@@ -1,8 +1,8 @@
 def task():
 
     # grade should be a tuple containing student name (str), subject (str) and score (int)
-    name = str(input("Enter a name"))
-    subject = str(input("Enter a subject"))
+    name = (input("Enter a name"))
+    subject = (input("Enter a subject"))
     score = int(input("Enter a score"))
 
 
